a1cbd90dae3f8f279c206d39f446e01d4afc20bd
vdella <vitor.origamer@gmail.com>
2022-07-17 00:33:15 -0300

    added docs

--- src/parsing/parser.py
+++ src/parsing/parser.py
@@ -3,9 +3,10 @@ from multiprocessing import Pipe
 
 
 def read_configs_from(filepath) -> tuple:
+    """Reads configs from :param filepath: and :returns a tuple with (process_quantity, pipe_map, operations)."""
     with open(resource_dir / filepath) as f:
         lines = f.readlines()
-        lines = [line.rstrip() for line in lines]
+        lines = [line.rstrip() for line in lines]  # Trims the \n at the end of the lines.
 
     process_qtt = __processes_qtt_from(lines[lines.index('#processes') + 1: lines.index('#pipes')])
     pipes: dict = __build_pipes_from(lines[lines.index('#pipes') + 1: lines.index('#operations')])
@@ -16,12 +17,15 @@ def read_configs_from(filepath) -> tuple:
 
 
 def __processes_qtt_from(config_lines):
+    """Digests info from :param config_lines in order to retrieve the process quantity."""
     qtt_as_list = config_lines
-    qtt_as_str = qtt_as_list.pop()
+    qtt_as_str = qtt_as_list.pop()  # Gets the single element of the list.
     return int(qtt_as_str)
 
 
 def __build_pipes_from(config_lines: list) -> dict:
+    """:returns a pipe dictionary indexed by the processes ID's.
+    Connections inside the dict follow a (src_end_point, dst_end_point) pattern."""
     pipes = dict()
 
     for line in config_lines:
@@ -32,6 +36,8 @@ def __build_pipes_from(config_lines: list) -> dict:
 
 
 def __digest_operations_from(config_lines: list, process_qtt: int) -> dict:
+    """Digests operations to be executed by each process during execution.
+    Operations are put inside a dictionary indexed by the process' ID."""
     operations = {i: [] for i in range(process_qtt)}
 
     now = -1
