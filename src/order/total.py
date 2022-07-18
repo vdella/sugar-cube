ee6e0351a194dd632f40762f8ac38bc6ae0b093e
vdella <vitor.origamer@gmail.com>
2022-07-18 03:21:20 -0300

    created TotalWorker to enhance module readability

--- src/order/total.py
+++ src/order/total.py
@@ -3,97 +3,87 @@ from datetime import datetime
 from src.parsing.parser import read_configs_from
 
 
-counters = list()
+class TotalWorker:
 
+    def __init__(self, serial, process_qtt):
+        self.counters = [0 for _ in range(process_qtt)]
+        self.serial = serial
 
-def counters_for(process_qtt: int):
-    return [0 for _ in range(process_qtt)]
+    @staticmethod
+    def process(target, args):
+        return Process(target=target, args=args)
 
+    def local_time(self):
+        """:returns: a concatenation of strings stating the logical and physical times for a process."""
+        process_info = 'Process {}\n'.format(self.serial)
+        logical_time = '> Logical time = {}\n'.format(self.counters)
+        physical_time = '> Physical time = {}ms'.format(datetime.now())
+        return process_info + logical_time + physical_time
 
-def local_time_for(pid):
-    """:returns: a concatenation of strings stating the logical and physical times for a process."""
-    process_info = 'Process {}\n'.format(pid)
-    logical_time = '> Logical time = {}\n'.format(counters)
-    physical_time = '> Physical time = {}ms'.format(datetime.now())
-    return process_info + logical_time + physical_time
+    def sync_time_to(self, timestamps: list):
+        for i, _ in enumerate(self.counters):
+            self.counters[i] = max(self.counters[i], timestamps[i])
 
+            if i == self.serial:
+                self.counters[i] += 1
 
-def sync_time_to(identifier, timestamps: list):
-    global counters
+        return self.counters
 
-    for i, _ in enumerate(counters):
-        counters[i] = max(counters[i], timestamps[i])
+    def event(self):
+        """Abstract event, as some internal operation only known by the process that executes it.
+        Updates its clock."""
+        self.counters[self.serial] += 1
+        print('Event has happened in {}! Time = {}\n'.format(self.serial + 1, self.counters))
 
-        if i == identifier:
-            counters[i] += 1
+    def broadcast(self, content, pipes):
+        self.counters[self.serial] += 1
 
-    return counters
+        for pipe in pipes:
+            pipe.send((content, self.serial, self.counters))
 
+        print('Message \'{}\' from {}! Time: {}\n'.format(content, self.serial + 1, self.counters))
 
-def event(identifier):
-    """Abstract event, as some internal operation only known by the process that executes it.
-    Updates its clock."""
-    global counters
-    counters[identifier] += 1
-    print('Event has happened in {}! Time = {}\n'.format(identifier + 1, counters))
+    def deliver(self, pipe):
+        """Receives a message through a :param pipe. Updates the internal clock."""
+        message, sender_pid, timestamp = pipe.recv()
 
+        self.counters = self.sync_time_to(timestamp)
 
-def broadcast(content, pipes, identifier):
-    global counters
-    counters[identifier] += 1
+        print('\'{}\' from {} to {}! Time: {}\n'.format(message, sender_pid + 1, self.serial + 1, self.counters))
+        return
 
-    for pipe in pipes:
-        pipe.send((content, identifier, counters))
+    def process_one(self, pipe12, pipe13):
+        self.event()
+        self.broadcast('Hello!', [pipe12])
+        self.event()
+        self.deliver(pipe12)
+        self.broadcast('Yes.', [pipe13])
 
-    print('Message \'{}\' from {}! Time: {}\n'.format(content, identifier + 1, counters))
+    def process_two(self, pipe21, pipe23):
+        self.deliver(pipe23)
+        self.deliver(pipe21)
+        self.broadcast('A!', [pipe21])
 
-
-def deliver(pipe, identifier):
-    """Receives a message through a :param pipe. Updates the internal clock."""
-    message, sender_pid, timestamp = pipe.recv()
-
-    global counters
-    counters = sync_time_to(identifier, timestamp)
-
-    print('\'{}\' from {} to {}! Time: {}\n'.format(message, sender_pid + 1, identifier + 1, counters))
-    return
-
-
-def process_one(pipe12, pipe13, identifier):
-    event(identifier)
-    broadcast('Hello!', [pipe12], identifier)
-    event(identifier)
-    deliver(pipe12, identifier)
-    broadcast('Yes.', [pipe13], identifier)
-
-
-def process_two(pipe21, pipe23, identifier):
-    deliver(pipe23, identifier)
-    deliver(pipe21, identifier)
-    broadcast('A!', [pipe21], identifier)
-
-
-def process_three(pipe32, pipe13, identifier):
-    broadcast('Hell yeah!', [pipe32], identifier)
-    event(identifier)
-    deliver(pipe13, identifier)
+    def process_three(self, pipe32, pipe13):
+        self.broadcast('Hell yeah!', [pipe32])
+        self.event()
+        self.deliver(pipe13)
 
 
 if __name__ == '__main__':
     qtt, pipes = read_configs_from('config.txt')
 
-    counters = counters_for(qtt)
-
     one_and_two, two_and_one = pipes[(0, 1)]
     two_and_three, three_and_two = pipes[(1, 2)]
     one_and_three, three_and_one = pipes[(0, 2)]
 
-    process1 = Process(target=process_one,
-                       args=(one_and_two, one_and_three, 0))
-    process2 = Process(target=process_two,
-                       args=(two_and_one, two_and_three, 1))
-    process3 = Process(target=process_three,
-                       args=(three_and_two, three_and_one, 2))
+    worker1 = TotalWorker(0, 3)
+    worker2 = TotalWorker(1, 3)
+    worker3 = TotalWorker(2, 3)
+
+    process1 = worker1.process(target=worker1.process_one, args=(one_and_two, one_and_three))
+    process2 = worker2.process(target=worker2.process_two, args=(two_and_one, two_and_three))
+    process3 = worker3.process(target=worker3.process_three, args=(three_and_two, three_and_one))
 
     process1.start()
     process2.start()
