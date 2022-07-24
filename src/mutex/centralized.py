d35dd8bee189d454fc0bfad8410e6b083a46af77
vdella <vitor.origamer@gmail.com>
2022-07-24 04:47:43 -0300

    algorithm to be finished

---
+++ src/mutex/centralized.py
@@ -0,0 +1,10 @@
+from src.order.total import TotalWorker
+
+
+def atomize_for(worker: TotalWorker):
+    worker.state = 'WANTED'
+    message = (worker.counter, worker.serial)
+
+    worker.broadcast(message, list())  # TODO gather all pipe entries and broadcast through them.
+
+    worker.state = 'HELD'
